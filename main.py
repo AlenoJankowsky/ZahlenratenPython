7dd04feca154dc0f0feead22e9931da2bf9d5197
Aleno <aleno.jankowsky@5minds.de>
2022-11-18 16:23:46 +0100

    Inital commit for main game

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import random
 
-userName = input("Bitte geben Sie Ihren Benutzernamen ein: ")
+user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
+print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen 1 und 20. Welche Zahl ist es?")
 
-print(f"Hallo {userName}! Ich denke an eine Zahl zwischen 1 und 20. Welche Zahl ist es?")
+def main_game():
+    random_number = random.randint(1, 20)
+    guess_is_in_interval = guess > 0 and guess < 21
+
+    while not guess_is_in_interval:
+        guess = int(input("Bitte geben Sie eine Zahl zwischen 1 und 20 ein: "))
+       
+main_game()
