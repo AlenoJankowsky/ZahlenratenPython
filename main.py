a5edcce807a634f2a897adf8024b8c0d8b26a781
Aleno <aleno.jankowsky@5minds.de>
2022-11-21 08:58:05 +0100

    Move validation process into function

--- main.py
+++ main.py
@@ -3,19 +3,24 @@ import random
 # user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
 # print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen 1 und 20. Welche Zahl ist es?")
 
-def main_game():
-    random_number = random.randint(1, 20)
+def try_input(): 
     while True:
-        try: 
-            guess = int(input("Bitte geben Sie eine Zahl zwischen 1 und 20 ein: "))
-            if guess < 1 or guess > 20:
-                raise ValueError
-        except ValueError:
-            print("Eingabe unzulaessig. ")
-            continue
-        else:
-            break
-    
-    
+            try: 
+                guess = int(input("Bitte geben Sie eine Zahl zwischen 1 und 20 ein: "))
+                if (guess < 1 or guess > 20):
+                    raise ValueError
+            except ValueError:
+                print("Eingabe unzulaessig. ")
+                continue
+            else:
+                return guess
 
+def main_game():
+    while True: 
+        random_number = random.randint(1, 20)
+        amount_of_tries = 0
+        guess = try_input()
+
+        break
+        
 main_game()
