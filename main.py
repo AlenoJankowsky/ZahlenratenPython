6852ab286b6e2e2cc6cfa3af1f4895f05851cf6f
Aleno <aleno.jankowsky@5minds.de>
2022-11-21 11:08:21 +0100

    Add variables for magic numbers

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import random
 
+min_random_number_limit = 5
+max_random_number_limit = 19
+
 def welcome_username_prompt(): 
     user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
-    print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen 1 und 20. Welche Zahl ist es?")
+    print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen {min_random_number_limit} und {max_random_number_limit}. Welche Zahl ist es?")
 
 def try_input():
     while True:
         try: 
-            guess = int(input("Bitte geben Sie eine Zahl zwischen 1 und 20 ein: "))
-            if (1 > guess > 20):
+            guess = int(input(f"Bitte geben Sie eine Zahl zwischen {min_random_number_limit} und {max_random_number_limit} ein: "))
+            if (min_random_number_limit > guess or guess > max_random_number_limit):
                 raise ValueError
         except ValueError:
             print("Eingabe unzulaessig. ", end="")
@@ -34,7 +37,7 @@ def guess_is_correct(random_number, guess, amount_of_tries):
     
 def main_game():
         welcome_username_prompt()
-        random_number = random.randint(1, 20)
+        random_number = random.randint(min_random_number_limit, max_random_number_limit)
         amount_of_tries = 1
         guess = try_input()
         while not guess_is_correct(random_number, guess, amount_of_tries):
