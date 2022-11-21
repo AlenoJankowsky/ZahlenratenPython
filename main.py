0c6df1ae7ed84cc425c6437ca69a4691daa0bceb
Aleno <aleno.jankowsky@5minds.de>
2022-11-21 16:23:43 +0100

    Implement main feature

--- main.py
+++ main.py
@@ -4,45 +4,72 @@ MIN_NUM = 1
 MAX_NUM = 100
 BOUNDARY_VALUE = 10
 
-def welcome_username_prompt(): 
-    user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
-    print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen {MIN_NUM} und {MAX_NUM}. Welche Zahl ist es?")
 
-def try_input():
+def welcome_username_prompt():
+    user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
+    print(
+        f"Hallo {user_name}! Ich denke an eine Zahl zwischen {MIN_NUM} und {MAX_NUM}. Welche Zahl ist es?"
+    )
+
+
+def prompt_input():
     while True:
-        try: 
-            guess = int(input(f"Bitte geben Sie eine Zahl zwischen {MIN_NUM} und {MAX_NUM} ein: "))
-            if (MIN_NUM > guess or guess > MAX_NUM):
+        try:
+            guess = int(
+                input(
+                    f"Bitte geben Sie eine Zahl zwischen {MIN_NUM} und {MAX_NUM} ein: "
+                )
+            )
+            if MIN_NUM > guess or guess > MAX_NUM:
                 raise ValueError
             return guess
         except ValueError:
             print("Eingabe unzulaessig. ", end="")
             continue
 
+
 def guess_is_correct(random_number, guess, amount_of_tries):
-    if guess > random_number:
-        print("Dein Versuch ist zu hoch. Bitte versuche es nochmal: ", end="")
+    guess_is_out_of_bound = (
+        guess > random_number + BOUNDARY_VALUE or guess < random_number - BOUNDARY_VALUE
+    )
+    first_guess_is_in_lower_bound = (
+        guess > random_number - BOUNDARY_VALUE and guess < random_number
+    )
+    first_guess_is_in_upper_bound = (
+        guess < random_number + BOUNDARY_VALUE and guess > random_number
+    )
+    first_guess_is_in_bound = (
+        first_guess_is_in_lower_bound or first_guess_is_in_upper_bound
+    )
+    is_first_check = True
+
+    if guess_is_out_of_bound and is_first_check:
+        print("Kalt. ", end="")
+        is_first_check = False
 
         return False
-    
-    if guess < random_number: 
-        print("Dein Versuch ist zu niedrig. Bitte versuche es nochmal: ", end="")
+
+    if first_guess_is_in_bound and is_first_check:
+        print("Warm. ", end="")
+        is_first_check = False
 
         return False
-    
+
     if guess == random_number:
         print(f"Wow. Punktlandung. :) Du hast {amount_of_tries} Versuche gebraucht.")
 
         return True
-    
-def main_game():
-        welcome_username_prompt()
-        random_number = random.randint(MIN_NUM, MAX_NUM)
-        amount_of_tries = 1
-        guess = try_input()
-        while not guess_is_correct(random_number, guess, amount_of_tries):
-            guess = try_input()
-            amount_of_tries += 1
 
-if __name__ == '__main__':
+
+def main_game():
+    welcome_username_prompt()
+    random_number = random.randint(MIN_NUM, MAX_NUM)
+    amount_of_tries = 1
+    guess = prompt_input()
+    while not guess_is_correct(random_number, guess, amount_of_tries):
+        guess = prompt_input()
+        amount_of_tries += 1
+
+
+if __name__ == "__main__":
     main_game()
