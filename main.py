57f274cede0ed7c6c784d0032a7842cbe526a365
Aleno <aleno.jankowsky@5minds.de>
2022-11-21 08:50:50 +0100

    Add input validation

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import random
 
-user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
-print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen 1 und 20. Welche Zahl ist es?")
+# user_name = input("Bitte geben Sie Ihren Benutzernamen ein: ")
+# print(f"Hallo {user_name}! Ich denke an eine Zahl zwischen 1 und 20. Welche Zahl ist es?")
 
 def main_game():
     random_number = random.randint(1, 20)
-    guess_is_in_interval = guess > 0 and guess < 21
+    while True:
+        try: 
+            guess = int(input("Bitte geben Sie eine Zahl zwischen 1 und 20 ein: "))
+            if guess < 1 or guess > 20:
+                raise ValueError
+        except ValueError:
+            print("Eingabe unzulaessig. ")
+            continue
+        else:
+            break
+    
+    
 
-    while not guess_is_in_interval:
-        guess = int(input("Bitte geben Sie eine Zahl zwischen 1 und 20 ein: "))
-       
 main_game()
